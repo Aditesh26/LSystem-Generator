315c78fff260ab593a22a6637681517910a07c9a
Aditesh Patro <aditeshpatro@gmail.com>
2026-01-18 18:47:46 +0530

    Final changes and polishing done

--- main.py
+++ main.py
@@ -21,40 +21,6 @@ t = turtle.RawTurtle(screen)
 t.speed(0)
 t.left(90)
 
-def generate():
-    t.clear()
-    t.penup()
-    t.home()
-    t.heading(90)
-    t.pendown()
-
-    rules = {
-        "X": "F[+X][-X]FX",
-        "F": "FF"
-    }
-
-    axiom = "X"
-    iterations = 4
-    angle = 25
-    steps = 5
-    s = expand_lsystem(axiom, rules, iterations)
-    draw(t, s, angle, steps)
-    
-    screen.update()
-
-
-generate_btn = tk.Button(
-    controls_frame,
-    text="Generate",
-    command= generate()
-)
-generate_btn.pack(pady=20)
-
-
-root.mainloop()
-
-t = turtle.Turtle()
-t.speed(10)
 def expand_lsystem(axiom, rules, iterations):
     current = axiom
     for i in range(iterations):
@@ -67,11 +33,12 @@ def expand_lsystem(axiom, rules, iterations):
         current = next_string
     return current
 
-print(expand_lsystem("F", rules, 2))
 
 def draw(t, instructions, angle, steps = 5):
 
     history = []
+
+    depth = 0
 
     for command in instructions:
         if command == "F":
@@ -82,17 +49,104 @@ def draw(t, instructions, angle, steps = 5):
             t.left(angle)
         elif command == "[":
             history.append((t.position(), t.heading()))
+            depth+=1
         elif command == "]":
             position, heading = history.pop()
             t.penup()
             t.setposition(position)
             t.setheading(heading)
             t.pendown()
+        t.pencolor(0, max(0.3, 1 - depth * 0.1), 0)
 
 
 
-t.left(90)
-s = expand_lsystem("X", rules, 4)
-draw(t, s, 25,10)
+def generate():
+    t.clear()
+    t.penup()
+    t.home()
+    t.setheading(90)
+    t.pendown()
+
+    rules = {
+        "X": "F[+X][-X]FX",
+        "F": "FF"
+    }
+
+    try:
+        axiom = axiom_entry.get()
+        iterations = int(iter_entry.get())
+        angle = float(angle_entry.get())
+        steps = int(step_entry.get())
+    except ValueError:
+        print("Invalid input")
+        return
+
+    if iterations > 7:
+        print("Warning: High iteration count may freeze the app")
+        return
+    s = expand_lsystem(axiom, rules, iterations)
+    draw(t, s, angle, steps)
+    
+    screen.update()
+
+def reset():
+    t.clear()
+    t.penup()
+    t.home()
+    t.setheading(90)
+    t.pendown()
+    screen.update()
+    
 
 
+
+tk.Label(controls_frame, text="Axiom").pack(pady=(10, 0))
+axiom_entry = tk.Entry(controls_frame)
+axiom_entry.pack()
+axiom_entry.insert(0, "X")
+
+
+tk.Label(controls_frame, text="Iterations").pack(pady=(10, 0))
+iter_entry = tk.Entry(controls_frame)
+iter_entry.pack()
+iter_entry.insert(0, "4")
+
+
+tk.Label(controls_frame, text="Angle").pack(pady=(10, 0))
+angle_entry = tk.Entry(controls_frame)
+angle_entry.pack()
+angle_entry.insert(0, "25")
+
+
+tk.Label(controls_frame, text="Step Size").pack(pady=(10, 0))
+step_entry = tk.Entry(controls_frame)
+step_entry.pack()
+step_entry.insert(0, "5")
+
+
+tk.Label(controls_frame, text="Rules").pack(pady=(10, 0))
+rules_text = tk.Text(controls_frame, height=5, width=25)
+rules_text.pack()
+
+rules_text.insert(
+    "1.0",
+    "X:F[+X][-X]FX\nF:FF"
+)
+
+
+
+generate_btn = tk.Button(
+    controls_frame,
+    text="Generate",
+    command= generate
+)
+generate_btn.pack(pady=20)
+
+reset_btn = tk.Button(
+    controls_frame,
+    text="Reset",
+    command= reset
+)
+reset_btn.pack(pady=10)
+
+root.mainloop()
