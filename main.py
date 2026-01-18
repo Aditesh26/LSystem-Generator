7a75a165e4dfdd6be81e3c1bbfb7611b57c8c32a
Aditesh Patro <aditeshpatro@gmail.com>
2026-01-18 17:23:34 +0530

    some updates to the L-system draw function

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import turtle
 
 t = turtle.Turtle()
-t.speed(0)
+t.speed(10)
 def expand_lsystem(axiom, rules, iterations):
     current = axiom
     for i in range(iterations):
@@ -13,10 +13,16 @@ def expand_lsystem(axiom, rules, iterations):
                 next_string += symbol
         current = next_string
     return current
-rules = {"F": "F+F--F+F"}
+rules = {
+    "X": "F[+X][-X]FX",
+    "F": "FF"
+}
 print(expand_lsystem("F", rules, 2))
 
 def draw(t, instructions, angle, steps = 5):
+
+    history = []
+
     for command in instructions:
         if command == "F":
             t.forward(steps)
@@ -24,6 +30,16 @@ def draw(t, instructions, angle, steps = 5):
             t.right(angle)
         elif command == "-":
             t.left(angle)
+        elif command == "[":
+            history.append((t.position(), t.heading()))
+        elif command == "]":
+            position, heading = history.pop()
+            t.penup()
+            t.setposition(position)
+            t.setheading(heading)
+            t.pendown()
 
-draw(t, "F+F--F+F", 60,100)
+t.left(90)
+s = expand_lsystem("X", rules, 4)
+draw(t, s, 25,10)
 turtle.done()
